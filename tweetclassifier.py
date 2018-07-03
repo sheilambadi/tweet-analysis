b3e64922d941279b7ef05395483a83cf21ce608c
sheilambadi <mbadiatieno@gmail.com>
2018-07-03 15:29:48 +0300

    Add word stemming

--- tweetclassifier.py
+++ tweetclassifier.py
@@ -1,14 +1,32 @@
 from setup import *
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 def getTweets():
     results = filterTweets("#amzn")
     return results
     
 def preprocessTweets():
-    tweetResults = getTweets()    
+    tweetResults = getTweets()   
+    stop_words = set(stopwords.words('english')) 
+    ps = PorterStemmer()
 
     for tweet in tweetResults:
-        print(word_tokenize(str(tweet)))
+        # word tokenization
+        word_tokens = word_tokenize(str(tweet))
+
+        # stop words removals
+        filtered_sentence = [w for w in word_tokens if not w in stop_words]
+
+        filtered_sentence = []
+
+        for w in word_tokens:
+            if w not in stop_words:
+                # stemming for normalization purpose
+                stemmed_word = ps.stem(w)
+                filtered_sentence.append(stemmed_word)
+
+        print(filtered_sentence)
 
 preprocessTweets()
